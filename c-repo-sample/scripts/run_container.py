2c14cec9c2af8c648dcebdcfb9630c779c7d516c
Menachem Adin <meniadin@gmail.com>
2025-02-21 09:57:20 +0200

    Update file

--- c-repo-sample/scripts/run_container.py
+++ c-repo-sample/scripts/run_container.py
@@ -4,5 +4,5 @@ script = utils.SCRIPTS_DIR / 'build_docker_image.py'
 command = f'{utils.PYTHON_EXECUTABLE} {script}'
 utils.run_command(command, shell=True, check=True)
 
-command = f'docker run --name {utils.REPO_NAME}-cont --rm {utils.REPO_NAME}-img'
+command = f'docker run --name {utils.REPO_NAME}-cont -t --rm {utils.REPO_NAME}-img'
 utils.run_command(command, shell=True, check=True)
